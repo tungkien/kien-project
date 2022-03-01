95b6ec6f8a813b7b50c0157c50231550d619c672
Kien <tungkien2306@gmail.com>
2022-03-02 00:16:42 +0700

    code moi

--- Project4/Project4.js
+++ Project4/Project4.js
@@ -1,4 +1,4 @@
-fetch('https://gnews.io/api/v4/top-headlines?&token=41491e9dd437e9aafcafc00c414de0c2')
+fetch('https://gnews.io/api/v4/top-headlines?&token=fc3a7262e6e6000b644932c3e0957e47')
     .then(response => response.json()
     )
     .then(function (data) {
@@ -23,26 +23,64 @@ fetch('https://gnews.io/api/v4/top-headlines?&token=41491e9dd437e9aafcafc00c414d
     });
 
 $(document).ready(function(){
+    
     $(".modal__inner button").click(function(){
         $(".modal").hide()
     })
        
     $("#open-modal").click(function(){
-            $(".modal").show()
+        $(".modal").show()
     })
-    var keyword = $("#keyword").val()
-    console.log(keyword)
     $("#search").on("click",function(){
-        fetch(`https://gnews.io/api/v4/search?q=${keyword}&token=41491e9dd437e9aafcafc00c414de0c2`)
+        
+        
+        var keyword = $("#keyword").val()
+        fetch(`https://gnews.io/api/v4/search?q=${keyword}&token=fc3a7262e6e6000b644932c3e0957e47`)
             .then(function(response){
-                console.log(response.json())
+                $("#main").empty()
+                return response.json()
             })
             .then(function(data){
                 console.log(data)
+                var getArticle = data.articles
+                for(var i = 0; getArticle.length; i++){
+                    $("#main").append(
+                        `
+                    <div class = "main-content">
+                        <div class = "image">
+                            <img src = "${getArticle[i].image}">
+                        </div>
+                    
+                        <div class = "content">
+                            <h3 class = "title">${getArticle[i].title}</h4>
+                            <p class = "published">${getArticle[i].publishedAt}</p>
+                            <p class = "description">${getArticle[i].description}</p>       
+                        </div>
+                    </div> 
+                        `
+                    )
+                }
             })
-    })
-        
+        })
+})            
 
-})
+    
   
 
+ // $("main").show()
+                // for(var i = 0; i < data.articles.length; i++){
+                //     $("#main").append(
+                //         `
+                //         <div class = "main-content">
+                //     <div class = "image">
+                //         <img src = "${data.articles[i].image}">
+                //     </div>
+                //     <div class = x"content">
+                //         <h3 class = "title">${data.articles[i].title}</h4>
+                //         <p class = "published">${data.articles[i].publishedAt}</p>
+                //         <p class = "description">${data.articles[i].description}</p>       
+                //     </div>
+                // </div>  
+                //         `
+                //     )
+                
